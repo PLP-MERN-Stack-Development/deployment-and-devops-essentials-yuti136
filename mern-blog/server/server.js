c99bfc5a9959698e7b0c49817cba3602e6850f48
Euticus Kong'o <euticuskon19@gmail.com>
2025-11-17 14:41:06 +0300

    Fix CORS allowed origin URL format

--- mern-blog/server/server.js
+++ mern-blog/server/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://deployment-and-devops-essentials-yu-kohl.vercel.app/", // your Vercel frontend
+  "https://deployment-and-devops-essentials-yu-kohl.vercel.app", // your Vercel frontend
 ];
 
 app.use(
